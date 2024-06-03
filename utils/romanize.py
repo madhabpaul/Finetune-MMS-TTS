6db1b6f471f44972f75e68c2114816a4638759c1
Madhab Paul <madhab12paul@gmail.com>
2024-06-04 00:10:17 +0530

    Update romanize.py

--- utils/romanize.py
+++ utils/romanize.py
@@ -1,18 +1,80 @@
 import os
 import subprocess
 
+asm_roman = [
+    ('ঁ',   'n'),
+    ('ং',   'ng'),
+    ('ঃ',   'h'),
+    ('অ',   'a'),
+    ('আ',   'aa'),
+    ('ই',   'i'),
+    ('ঈ',   'ii'),
+    ('উ',   'u'),
+    ('ঋ',   'ra'),
+    ('এ',   'e'),
+    ('ঐ',   'ai'),
+    ('ও',   'o'),
+    ('ক',   'ka'),
+    ('খ',   'kha'),
+    ('গ',   'ga'),
+    ('ঘ',   'gha'),
+    ('ঙ',   'nga'),
+    ('চ',   'cha'),
+    ('ছ',   'chha'),
+    ('জ',   'ja'),
+    ('ঞ',   'nya'),
+    ('ট',   'tta'),
+    ('ঠ',   'ttha'),
+    ('ড',   'dda'),
+    ('ঢ',   'ddha'),
+    ('ণ',   'nna'),
+    ('ত',   'ta'),
+    ('থ',   'tha'),
+    ('দ',   'da'),
+    ('ধ',   'dha'),
+    ('ন',   'na'),
+    ('প',   'pa'),
+    ('ফ',   'pha'),
+    ('ব',   'ba'),
+    ('ভ',   'bha'),
+    ('ম',   'ma'),
+    ('য',   'ya'),
+    ('র',   'ba'),
+    ('ল',   'la'),
+    ('শ',   'sha'),
+    ('ষ',   'ssa'),
+    ('স',   'sa'),
+    ('হ',   'ha'),
+    ('া',   'aa'),
+    ('ি',   'i'),
+    ('ী',   'ii'),
+    ('ু',   'u'),
+    ('ূ',   'uu'),
+    ('ৃ',   'ra'),
+    ('ে',   'e'),
+    ('ৈ',   'ai'),
+    ('ো',   'o'),
+    ('ৌ',   'au'),
+    ('্',   ''),
+    ('ৎ',   'ta'),
+    ('ৰ',   'ra'),
+    ('ৱ',   'ba'),
+    ('য়',   'yya'),
+    ('০', 'xuinno'),
+    ('১', 'ak'),
+    ('২', 'dui'),
+    ('৩', 'tini'),
+    ('৪', 'sari'),
+    ('৫', 'pas'),
+    ('৬', 'soi'),
+    ('৭', 'xat'),
+    ('৮', 'ath'),
+    ('৯', 'nou'),
+    ('।', '  '),
+]
+
 def uromanize(input_string, uroman_path):
-    """Convert non-Roman strings to Roman using the `uroman` perl package."""
-    script_path = os.path.join(uroman_path, "bin", "uroman.pl")
-
-    command = ["perl", script_path]
-
-    process = subprocess.Popen(command, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    # Execute the perl command
-    stdout, stderr = process.communicate(input=input_string.encode())
-
-    if process.returncode != 0:
-        raise ValueError(f"Error {process.returncode}: {stderr.decode()}")
-
-    # Return the output as a string and skip the new-line character at the end
-    return stdout.decode()[:-1]
+     for src, dst in asm_roman:
+        inputs = input_string.replace(src, dst)
+    print (inputs)
+    return inputs
