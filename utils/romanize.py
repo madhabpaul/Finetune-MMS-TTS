76981001c71f703d5adf26d3948ea5adcd50688b
Madhab Paul <madhab12paul@gmail.com>
2024-06-05 12:35:05 +0530

    Update romanize.py

--- utils/romanize.py
+++ utils/romanize.py
@@ -1,80 +1,33 @@
 import os
 import subprocess
 
-asm_roman = [
-    ('ঁ',   'n'),
-    ('ং',   'ng'),
-    ('ঃ',   'h'),
-    ('অ',   'a'),
-    ('আ',   'aa'),
-    ('ই',   'i'),
-    ('ঈ',   'ii'),
-    ('উ',   'u'),
-    ('ঋ',   'ra'),
-    ('এ',   'e'),
-    ('ঐ',   'ai'),
-    ('ও',   'o'),
-    ('ক',   'ka'),
-    ('খ',   'kha'),
-    ('গ',   'ga'),
-    ('ঘ',   'gha'),
-    ('ঙ',   'nga'),
-    ('চ',   'cha'),
-    ('ছ',   'chha'),
-    ('জ',   'ja'),
-    ('ঞ',   'nya'),
-    ('ট',   'tta'),
-    ('ঠ',   'ttha'),
-    ('ড',   'dda'),
-    ('ঢ',   'ddha'),
-    ('ণ',   'nna'),
-    ('ত',   'ta'),
-    ('থ',   'tha'),
-    ('দ',   'da'),
-    ('ধ',   'dha'),
-    ('ন',   'na'),
-    ('প',   'pa'),
-    ('ফ',   'pha'),
-    ('ব',   'ba'),
-    ('ভ',   'bha'),
-    ('ম',   'ma'),
-    ('য',   'ya'),
-    ('র',   'ba'),
-    ('ল',   'la'),
-    ('শ',   'sha'),
-    ('ষ',   'ssa'),
-    ('স',   'sa'),
-    ('হ',   'ha'),
-    ('া',   'aa'),
-    ('ি',   'i'),
-    ('ী',   'ii'),
-    ('ু',   'u'),
-    ('ূ',   'uu'),
-    ('ৃ',   'ra'),
-    ('ে',   'e'),
-    ('ৈ',   'ai'),
-    ('ো',   'o'),
-    ('ৌ',   'au'),
-    ('্',   ''),
-    ('ৎ',   'ta'),
-    ('ৰ',   'ra'),
-    ('ৱ',   'ba'),
-    ('য়',   'yya'),
-    ('০', 'xuinno'),
-    ('১', 'ak'),
-    ('২', 'dui'),
-    ('৩', 'tini'),
-    ('৪', 'sari'),
-    ('৫', 'pas'),
-    ('৬', 'soi'),
-    ('৭', 'xat'),
-    ('৮', 'ath'),
-    ('৯', 'nou'),
-    ('।', '  '),
+asm_num = [
+    ('১', 'এক '),
+    ('২', 'দুই '),
+    ('৩', 'তিনি '),
+    ('৪', 'চাৰি '),
+    ('৫', 'পাঁচ '),
+    ('৬', 'ছয় '),
+    ('৭', 'সাত '),
+    ('৮', 'আঠ '),
+    ('৯', 'ন '),
+    ('০', 'শূন্য '),
 ]
 
 def uromanize(input_string, uroman_path):
-     for src, dst in asm_roman:
+    """Convert non-Roman strings to Roman using the `uroman` perl package."""
+    script_path = os.path.join(uroman_path, "bin", "uroman.pl")
+
+    command = ["perl", script_path]
+    for src, dst in asm_num:
         inputs = input_string.replace(src, dst)
-    print (inputs)
-    return inputs
+
+    process = subprocess.Popen(command, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    # Execute the perl command
+    stdout, stderr = process.communicate(input=inputs.encode())
+
+    if process.returncode != 0:
+        raise ValueError(f"Error {process.returncode}: {stderr.decode()}")
+
+    # Return the output as a string and skip the new-line character at the end
+    return stdout.decode()[:-1]
